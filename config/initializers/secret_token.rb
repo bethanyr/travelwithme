b8c5ce1e6c1ba42408d33709b4fd5670a6cd1b7f
bethanyr <bethany.rentz@hotmail.com>
2013-08-20 14:00:40 -0700

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelWithMe::Application.config.secret_token = '99039fa265009cff82afb108edcd98f18f8d42511e9a48f15ad97def4fa52bb7c746ffa8e18727b241011f199481590c71bc22f71224891a15319c2e9499980a'
